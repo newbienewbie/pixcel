035b1aa5cbfbfe8152617bb94750e55b307716db
newbienewbie <webbot@sina.com>
2017-04-20 13:51:24 +0800

    bugfix：修复excel另存为的错误

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const path=require('path');
 const {retrievePixelsFromImage} =require('./lib/pix');
 const {send}=require('./lib/comm');
 
-const filename=path.join("C:/Users/itminus/personal info/Sniper .jpg");
+const filename=path.join("C:/Users/itminus/personal info/1.png");
+const saveas=path.join("C:/Users/itminus/personal info/1.xlsx");
 
 retrievePixelsFromImage(filename)
     .then(pixels=>{
         const obj={
             pixels,
-            filename,
+            filename:saveas,
         };
         send(JSON.stringify(obj));
     })
